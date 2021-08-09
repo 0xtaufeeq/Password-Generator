20ee05e967dc54a509571ad9a2fc63b8af0dc842
Taufeeq Riyaz <taufeeq.riyaz64@gmail.com>
2021-08-10 01:37:04 +0530

    Update Password-Generator.py

--- Password-Generator.py
+++ Password-Generator.py
@@ -1,11 +1,11 @@
 import random
 
-lower = "abcdefghijklmnopqrstuvwxyz"
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
 symbols = "[]{}()*;/.,-_"
 
-all = lower + upper + numbers + symbols
-length = 12
+all = lowercase + uppercase + numbers + symbols
+length = 12 #Change this number to change the length of your password
 password = "".join(random.sample(all,length))
 print(password)
